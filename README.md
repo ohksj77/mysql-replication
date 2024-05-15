8539181c7d73f4dd8149c8b686bafc3d465e0300
김승진 <ohksj77@gmail.com>
2024-05-15 16:30:43 +0900

    transaction based gtid replica

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ docker-compose up -d
 ## 실행 결과
 - replication 관련 로그
 ![](https://github.com/ohksj77/mysql-replication/assets/89020004/1a1c53ef-3343-4416-984e-81101931bca7)
+- binary 로그를 확인하는 방법과 GTID를 활용한 트랜잭션 기반 방법 모두 실습 완료
 
 ## 추가 테스트한 부분
 - record 를 통한 dto 활용
@@ -23,10 +24,10 @@ docker-compose up -d
   - ![](https://github.com/ohksj77/mysql-replication/assets/89020004/bbb4d944-7a01-489c-8405-5d719ff7626c)
 - envelop 패턴을 위한 ResponseBodyAdvice 재정의
   - ![](https://github.com/ohksj77/mysql-replication/assets/89020004/faada3e2-0fbc-49f0-a900-983ba3288d5e)
-  - ApiResponse<T> 를 통해 envelope 패턴을 구현하고 다음과 같이 컨트롤러에서 명시 안해도 ApiResponse 로 래핑하여 반환됨을 확인함
   - 예제를 따라하느라 제네릭에 Object를 넣어 의미가 없어졌지만, 이후 제네릭스럽게 테스트 해보고자 함
   - @RestControllerAdvice 클래스 내부 메서드의 반환 또한 ApiResponse<T>로 래핑되어 반환됨을 확인함
   - ![](https://github.com/ohksj77/mysql-replication/assets/89020004/5ffe9a46-82ed-4aa6-a130-dad0e70057d6)
+  - ApiResponse<T> 를 통해 envelope 패턴을 구현하고 다음과 같이 컨트롤러에서 명시 안해도 ApiResponse 로 래핑하여 반환됨을 확인함
 - 간단한 cqrs 적용
   - 어노테이션 @Service와 @Transactional를 사용해 readOnly 설정을 조절하며 커스텀 어노테이션을 구성하여 서비스별로 접근하는 db를 다르게 가져감
   - query, command 패키지 분리
--- slave/entrypoint.sh
+++ slave/entrypoint.sh
@@ -13,17 +13,18 @@ export MYSQL_PWD='1234'
 # Master DB의 IP 주소 및 마스터 상태 정보를 변수로 설정
 MASTER_IP=$(getent hosts db_master | awk '{ print $1 }')
 MASTER_STATUS=$(mysql -u replicator -h $MASTER_IP -e "SHOW MASTER STATUS;")
-MASTER_LOG_FILE=$(echo "$MASTER_STATUS" | awk 'NR==2 {print $1}')
-MASTER_LOG_POS=$(echo "$MASTER_STATUS" | awk 'NR==2 {print $2}')
+# MASTER_LOG_FILE=$(echo "$MASTER_STATUS" | awk 'NR==2 {print $1}')
+# MASTER_LOG_POS=$(echo "$MASTER_STATUS" | awk 'NR==2 {print $2}')
 
 # 슬레이브에서 MySQL 복제 설정
-mysql -u replicator -h localhost -P 3306 -e "STOP SLAVE;"
-mysql -u replicator -h localhost -P 3306 -e "RESET SLAVE;"
-mysql -u replicator -h localhost -P 3306 -e "CHANGE MASTER TO MASTER_HOST='$MASTER_IP', MASTER_USER='replicator', MASTER_PASSWORD='1234', MASTER_LOG_FILE='$MASTER_LOG_FILE', MASTER_LOG_POS=$MASTER_LOG_POS, GET_MASTER_PUBLIC_KEY=1;"
-mysql -u replicator -h localhost -P 3306 -e "START SLAVE;"
+mysql -u replica -h localhost -P 3306 -e "STOP SLAVE;"
+mysql -u replica -h localhost -P 3306 -e "RESET SLAVE;"
+# mysql -u replicator -h localhost -P 3306 -e "CHANGE MASTER TO MASTER_HOST='$MASTER_IP', MASTER_USER='replicator', MASTER_PASSWORD='1234', MASTER_LOG_FILE='$MASTER_LOG_FILE', MASTER_LOG_POS=$MASTER_LOG_POS, GET_MASTER_PUBLIC_KEY=1;"
+mysql -u replica -h localhost -P 3306 -e "CHANGE MASTER TO MASTER_HOST='$MASTER_IP', MASTER_USER='replicator', MASTER_PASSWORD='1234', MASTER_AUTO_POSITION=1;"
+mysql -u replica -h localhost -P 3306 -e "START SLAVE;"
 
 # 복제 상태 확인
-mysql -u replicator -h localhost -P 3306 -e "SHOW SLAVE STATUS\G;"
+mysql -u replica -h localhost -P 3306 -e "SHOW SLAVE STATUS\G;"
 
 # MySQL 프로세스가 종료되지 않도록 대기
 wait $!
